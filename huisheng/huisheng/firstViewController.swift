bb56f828cadacecb7be5b8a275ebeb366ddc63e9
Andrew <super@AndrewdeMacBook-Air.local>
2018-08-29 22:03:37 +0800

    基本UI框架搭建,跳转等等

---
+++ huisheng/huisheng/firstViewController.swift
@@ -0,0 +1,37 @@
+//
+//  firstViewController.swift
+//  huisheng
+//
+//  Created by Andrew on 2018/8/29.
+//  Copyright © 2018年 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class firstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
