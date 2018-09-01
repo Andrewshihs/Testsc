ce7add9c384490b71adf6404b294c16439956906
Andrew <super@AndrewdeMacBook-Air.local>
2018-09-01 23:44:02 +0800

    照片搞怪页面架设完成,获取不到image,demo也是这样的情况,准备换一个demo进行实践

--- huisheng/huisheng/ViewController.swift
+++ huisheng/huisheng/ViewController.swift
@@ -7,26 +7,103 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+import MobileCoreServices
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
     @IBOutlet weak var CameraImage: UIImageView!
+    @IBOutlet weak var takePictureButton: UIButton!
+    var avPlayerViewController: AVPlayerViewController!
+    var image: UIImage?
+    var movieURL: URL?
+    var lastChosenMediaType: String?
+    
     @IBAction func Album(_ sender: UIButton) {
-        
+        pickMediaFromSource(UIImagePickerControllerSourceType.photoLibrary)
     }
     
     @IBAction func Camera(_ sender: UIButton) {
-        
+        pickMediaFromSource(UIImagePickerControllerSourceType.camera)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-         
         // Do any additional setup after loading the view, typically from a nib.
     }
     override func viewWillAppear(_ animated: Bool) {
     //    self.navigationController?.isNavigationBarHidden = false
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        updateDisplay()
+    }
+  
+    func updateDisplay() {
+        if let mediaType = lastChosenMediaType {
+            if mediaType == (kUTTypeImage as NSString) as String {
+                CameraImage.image = image!
+                CameraImage.isHidden = false
+                if avPlayerViewController != nil {
+                    avPlayerViewController!.view.isHidden = true
+                }
+            } else if mediaType == (kUTTypeMovie as NSString) as String {
+                if avPlayerViewController == nil {
+                    avPlayerViewController = AVPlayerViewController()
+                    let avPlayerView = avPlayerViewController!.view
+                    avPlayerView?.frame = CameraImage.frame
+                    avPlayerView?.clipsToBounds = true
+                    view.addSubview(avPlayerView!)
+                }
+                
+                if let url = movieURL {
+                    CameraImage.isHidden = true
+                    avPlayerViewController.player = AVPlayer(url: url)
+                    avPlayerViewController!.view.isHidden = false
+                    avPlayerViewController!.player!.play()
+                }
+            }
+        }
+    }
+    
+    func pickMediaFromSource(_ sourceType:UIImagePickerControllerSourceType) {
+        let mediaTypes =
+            UIImagePickerController.availableMediaTypes(for: sourceType)!
+        if UIImagePickerController.isSourceTypeAvailable(sourceType)
+            && mediaTypes.count > 0 {
+            let picker = UIImagePickerController()
+            picker.mediaTypes = mediaTypes
+           // picker.delegate = self
+            picker.allowsEditing = true
+            picker.sourceType = sourceType
+            present(picker, animated: true, completion: nil)
+        } else {
+            let alertController = UIAlertController(title:"Error accessing media",
+                                                    message: "Unsupported media source.",
+                                                    preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK",
+                                         style: UIAlertActionStyle.cancel, handler: nil)
+            alertController.addAction(okAction)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    private func imagePickerController(_ picker: UIImagePickerController,
+                                       didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        lastChosenMediaType = info[UIImagePickerControllerMediaType] as? String
+        if let mediaType = lastChosenMediaType {
+            if mediaType == (kUTTypeImage as NSString) as String {
+                image = info[UIImagePickerControllerEditedImage] as? UIImage
+            } else if mediaType == (kUTTypeMovie as NSString) as String {
+                movieURL = info[UIImagePickerControllerMediaURL] as? URL
+            }
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion:nil)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
